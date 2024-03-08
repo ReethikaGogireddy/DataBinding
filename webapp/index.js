51fc79b08ad079feb78e3a0d55a8c04f52d38765
ReethikaGogireddy <gogireddyreethika1@gmail.com>
2024-03-08 11:05:49 +0530

    creating a model

--- webapp/index.js
+++ webapp/index.js
@@ -1,12 +1,23 @@
-sap.ui.require(["sap/m/Text"], function (Text) {
-  "use strict";
+sap.ui.require(
+  ["sap/m/Text", "sap/ui/model/json/JSONModel"],
+  function (Text, JSONModel) {
+    "use strict";
 
-  // Attach an anonymous function to the SAPUI5 'init' event
-  sap.ui.getCore().attachInit(function () {
-    // Create a text UI element that displays a hardcoded text string
-    new Text({ text: "Hi, my name is Reethika Gogireddy" }).placeAt("content");
-  });
-});
+    // Attach an anonymous function to the SAPUI5 'init' event
+    sap.ui.getCore().attachInit(function () {
+      // create a JSON model that contains the name
+      var oModel = new JSONModel({
+        greetingText: "Hi,my name is Reethika",
+      });
+
+      sap.ui.getCore().setModel(oModel); // this is not a good practise
+      // in the above line we are simply saying that we are adding a model/table(of data) to application
+      new Text({ text: "Hi, my name is Reethika Gogireddy" }).placeAt(
+        "content"
+      );
+    });
+  }
+);
 
 /*sap.ui.getCore().attachInit(function () { ... }) ensures that your code
 (anything inside the function) doesn't run until the essential parts
